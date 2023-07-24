07ddb0f452577e0e0f61e211a2c044bcafe70571
Abdurrasheed <abdurrasheedfalaludaura@gmail.com>
2023-07-24 09:58:16 +0100

    timezone with flask-moment

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
-
+from flask_moment import Moment
 
 import logging
 from logging.handlers import SMTPHandler
@@ -23,6 +23,7 @@ login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 mail = Mail(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 from app import routes, models, errors
 
